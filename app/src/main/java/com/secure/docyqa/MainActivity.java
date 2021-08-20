0fd296e1a9ae370062208d08e4f709ce042a5beb
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2021-08-21 02:04:39 +0530

    Frontend Complete

--- app/src/main/java/com/secure/docyqa/MainActivity.java
+++ app/src/main/java/com/secure/docyqa/MainActivity.java
@@ -2,13 +2,31 @@ package com.secure.docyqa;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button upload;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        init();
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, question_answer.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    void init(){
+        upload = findViewById(R.id.upload);
     }
 }
---
+++ app/src/main/java/com/secure/docyqa/question_answer.java
@@ -0,0 +1,14 @@
+package com.secure.docyqa;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class question_answer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_question_answer);
+    }
+}
