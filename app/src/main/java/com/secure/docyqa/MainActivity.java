d77898cc14721edf8bc21bd84c22169eb89ed9a3
SatyamSoni23 <2018kucp1121@iiitkota.ac.in>
2021-08-23 20:09:17 +0530

    Frontend Complete

--- app/src/main/java/com/secure/docyqa/MainActivity.java
+++ app/src/main/java/com/secure/docyqa/MainActivity.java
@@ -6,10 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+
+import com.google.android.material.textview.MaterialTextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button upload;
+    ImageView choose_image;
+    MaterialTextView pdf_name;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,5 +34,7 @@ public class MainActivity extends AppCompatActivity {
 
     void init(){
         upload = findViewById(R.id.upload);
+        choose_image = findViewById(R.id.choose_image);
+        pdf_name = findViewById(R.id.pdf_name);
     }
 }
--- app/src/main/java/com/secure/docyqa/question_answer.java
+++ app/src/main/java/com/secure/docyqa/question_answer.java
@@ -1,14 +1,33 @@
 package com.secure.docyqa;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
 
+import com.google.android.material.button.MaterialButton;
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textview.MaterialTextView;
+
 public class question_answer extends AppCompatActivity {
+
+    ConstraintLayout buffer;
+    TextInputEditText question;
+    MaterialTextView answer;
+    MaterialButton answer_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_answer);
+
+        init();
+    }
+
+    void init(){
+        buffer = findViewById(R.id.buffer);
+        question = findViewById(R.id.question);
+        answer = findViewById(R.id.answer);
+        answer_btn = findViewById(R.id.answer_btn);
     }
 }
