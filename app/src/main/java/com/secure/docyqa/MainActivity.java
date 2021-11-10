490aea1d7d62a909c186659af5fc426306305218
Satyam <2018kucp1121@iiitkota.ac.in>
2021-11-10 17:46:56 +0530

    Updated

--- app/src/main/java/com/secure/docyqa/MainActivity.java
+++ app/src/main/java/com/secure/docyqa/MainActivity.java
@@ -1,40 +1,93 @@
 package com.secure.docyqa;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
+import android.widget.Toast;
 
-import com.google.android.material.textview.MaterialTextView;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.secure.docyqa.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button upload;
-    ImageView choose_image;
-    MaterialTextView pdf_name;
+    ActivityMainBinding binding;
+    Uri imageUri = null;
+    ProgressDialog dialog;
+    public static String file_name = "null";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        init();
+        Intent gallaryIntent = new Intent();
 
-        upload.setOnClickListener(new View.OnClickListener() {
+        binding.chooseImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, question_answer.class);
-                startActivity(intent);
+                gallaryIntent.setAction(Intent.ACTION_GET_CONTENT);
+                gallaryIntent.setType("application/pdf");
+                startActivityForResult(gallaryIntent, 1);
             }
         });
     }
 
-    void init(){
-        upload = findViewById(R.id.upload);
-        choose_image = findViewById(R.id.choose_image);
-        pdf_name = findViewById(R.id.pdf_name);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(resultCode == RESULT_OK){
+            dialog = new ProgressDialog(this);
+            dialog.setMessage("Uploading...");
+            dialog.show();
+            imageUri = data.getData();
+            final String timeStamp = "" + System.currentTimeMillis();
+            StorageReference storageReference = FirebaseStorage.getInstance().getReference();
+            final String messagePushID = timeStamp;
+            Log.d("TEST_UPLOAD", imageUri.toString());
+            Log.d("TEST_UPLOAD", messagePushID);
+            final StorageReference filepath = storageReference.child("docs").child(messagePushID + "." + "pdf");
+            filepath.putFile(imageUri).continueWithTask(new Continuation() {
+                @Override
+                public Object then(@NonNull Task task) throws Exception {
+                    if (!task.isSuccessful()) {
+                        throw task.getException();
+                    }
+                    return filepath.getDownloadUrl();
+                }
+            }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+                @Override
+                public void onComplete(@NonNull Task<Uri> task) {
+                    if (task.isSuccessful()) {
+                        dialog.dismiss();
+                        Uri uri = task.getResult();
+                        String myurl;
+                        myurl = uri.toString();
+                        file_name = messagePushID;
+                        Toast.makeText(MainActivity.this, "Uploaded Successfully", Toast.LENGTH_SHORT).show();
+                        next_step();
+                    } else {
+                        dialog.dismiss();
+                        Toast.makeText(MainActivity.this, "UploadedFailed", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+        }
+    }
+    private void next_step(){
+        Intent intent = new Intent(this, question_answer.class);
+        startActivity(intent);
     }
 }
--- app/src/main/java/com/secure/docyqa/question_answer.java
+++ app/src/main/java/com/secure/docyqa/question_answer.java
@@ -2,32 +2,42 @@ package com.secure.docyqa;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.material.button.MaterialButton;
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textview.MaterialTextView;
+import com.secure.docyqa.databinding.ActivityQuestionAnswerBinding;
 
 public class question_answer extends AppCompatActivity {
 
-    ConstraintLayout buffer;
-    TextInputEditText question;
-    MaterialTextView answer;
-    MaterialButton answer_btn;
+    ActivityQuestionAnswerBinding binding;
+    String file_name = MainActivity.file_name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_question_answer);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_question_answer);
 
-        init();
+        binding.answerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(TextUtils.isEmpty(binding.question.getText().toString())){
+                    binding.question.setError("Please enter question");
+                    return;
+                }
+                String question = binding.question.getText().toString();
+                start_network_call(question);
+            }
+        });
     }
 
-    void init(){
-        buffer = findViewById(R.id.buffer);
-        question = findViewById(R.id.question);
-        answer = findViewById(R.id.answer);
-        answer_btn = findViewById(R.id.answer_btn);
+    private void start_network_call(String question){
+        //file_name, question;
     }
 }
